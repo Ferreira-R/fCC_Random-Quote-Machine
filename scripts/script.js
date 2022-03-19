9c0b24b24e7432f81eb32115d35371a088f99e3a
Ferreira-R <rrferreira99@outlook.com>
2022-03-18 23:21:54 -0500

    Animation test

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,18 @@
+const colors = [
+	'#16a085',
+	'#27ae60',
+	'#2c3e50',
+	'#f39c12',
+	'#e74c3c',
+	'#9b59b6',
+	'#FB6964',
+	'#342224',
+	'#472E32',
+	'#BDBB99',
+	'#77B1A9',
+	'#73A857',
+];
+
 // Powered by Quotable
 // https://github.com/lukePeavey/quotable
 
@@ -8,6 +23,7 @@ document.addEventListener('DOMContentLoaded', () => {
 	const quoteAuthor = document.querySelector('#author');
 
 	async function updateQuote() {
+		button.classList.remove('aniBounce');
 		// Fetch a random quote from the Quotable API
 		const response = await fetch('https://api.quotable.io/random');
 		const data = await response.json();
@@ -19,6 +35,7 @@ document.addEventListener('DOMContentLoaded', () => {
 			quote.textContent = 'An error occured';
 			console.log(data);
 		}
+		button.classList.add('aniBounce');
 	}
 
 	// Attach an event listener to the `button`
